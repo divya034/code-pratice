30ca79b59caade43bed43e6434bbafca9ebe89a6
divya034 <59786992+divya034@users.noreply.github.com>
2024-06-16 06:09:04 +0530

    Update and rename single_ele_sorted_array to corrected_ single_ele_sorted_array.cpp

--- Binary Search/corrected_ single_ele_sorted_array.cpp
+++ Binary Search/corrected_ single_ele_sorted_array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// https://leetcode.com/problems/single-element-in-a-sorted-array/description/
     int singleNonDuplicate(vector<int>& a) {
         int s = 0;
         int n = a.size() - 1;
@@ -24,9 +25,11 @@ public:
             if(mid + 1 <= n){
                 rightval = a[mid + 1];
             }
-
-            if(mid - 1 >= 0 && mid + 1 <= n && rightval != a[mid] && leftval != a[mid]){
-                return a[mid];
+// mid - 1 >= 0 && mid + 1 <= n &&  removed from the below condition as the
+// corner odd ele cases won't go inside the loop
+// ie when mid is at 0th index : mid - 1 won't work
+            if(rightval != a[mid] && leftval != a[mid]){
+                return curr;
             }
 
             else if(leftval == a[mid] && leftval != rightval){
