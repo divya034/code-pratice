69a23a0b9d5eb0454dd10769e004b996500eecfe
divya034 <59786992+divya034@users.noreply.github.com>
2024-06-26 15:16:33 +0530

    Update isomorphic_myans.cpp

--- Strings/isomorphic_myans.cpp
+++ Strings/isomorphic_myans.cpp
@@ -11,11 +11,35 @@ public:
             map_string[s[i]] = t[i];
             taken[t[i]] = 1;
             }
+            //present s is not mapped to present t, that means s mapping was taken by something else or t was already taken before
+            if(map_string[s[i]] != t[i]){
+                return false;
+            }
+        }
+    }
+
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int map_string[256] = {0};
+        int taken[256] = {0};
+        
+        // perform mapping for all elements in s to elements in t
+        // and mapping t to s to check if t element is taken
+        for(int i = 0; i < s.size(); i++){
+            if(map_string[s[i]] == 0 && taken[t[i]] == 0){
+            map_string[s[i]] = t[i];
+            taken[t[i]] = 1;
+            }
             //present s is not mapped to present t, that means s mapping was taken by something else
             if(map_string[s[i]] != t[i]){
                 return false;
             }
-            //t is taken
+// t is taken, so s is not mapped to anything
             else if(map_string[s[i]] == 0){
                return false;
             }
