1a9438af8d050d7fe474f915bdc0ce300afb0ebe
divya034 <59786992+divya034@users.noreply.github.com>
2024-07-19 15:50:37 +0530

    Update add_string using recursion.cpp

--- recursion/add_string using recursion.cpp
+++ recursion/add_string using recursion.cpp
@@ -52,3 +52,47 @@ public:
        return ans;
     }
 };
+
+
+ORRRRRRR
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string result = "";
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += num1[i] - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += num2[j] - '0';
+                j--;
+            }
+            carry = sum / 10;
+            result += to_string(sum % 10);
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    string num1 = "123";
+    string num2 = "456";
+    cout << solution.addStrings(num1, num2) << endl; // Output should be "579"
+    return 0;
+}
+
