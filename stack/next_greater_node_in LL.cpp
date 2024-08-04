f6c925487294103edc4c4bb9cb7731a2f02cdbc3
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-05 00:40:47 +0530

    Update next_greater_node_in LL.cpp

--- stack/next_greater_node_in LL.cpp
+++ stack/next_greater_node_in LL.cpp
@@ -1,4 +1,7 @@
-    pair<ListNode*, int> reversell(ListNode* head){
+class Solution {
+public:
+
+   pair<ListNode*, int> reversell(ListNode* head){
         ListNode* prev = nullptr;
         ListNode* curr = head;
         int count = 0;
@@ -34,6 +37,64 @@
 
             s.push(temp->val);
             temp = temp->next;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+MY SOLUTION BY ITERATING OVER LL INSTEAD OF COUNT
+
+
+class Solution {
+public:
+
+    pair<ListNode*, int> reversell(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forw = curr->next;
+        int count = 0;
+
+        while(curr != NULL){
+           forw = curr ->next;
+
+            curr->next = prev;
+            prev = curr;
+            curr = forw;
+            count++;
+
+        }
+
+        return {prev,count};
+    }
+    vector<int> nextLargerNodes(ListNode* head) {
+        stack<int> s;
+        // reverse because we need to traverse from end
+        auto [reversedHead, count] = reversell(head);
+
+        ListNode* temp = reversedHead;
+        vector<int> ans(count);
+
+        int i = 0;
+
+        while(temp != NULL){
+           while(!s.empty() && s.top() <= temp->val){
+            s.pop();
+           }
+           
+        //    mistake didnt write this condition
+            if (!s.empty()) {
+                ans[i] = s.top();
+            }
+            else{
+                ans[i] = 0;
+            }
+            
+           i++;
+           s.push(temp->val);
+           temp = temp->next;
         }
 
         reverse(ans.begin(), ans.end());
