bd512cac1a49b91139c9c8e8931b895823daaa3a
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-08 15:49:19 +0530

    Create simplify_path.cpp

---
+++ stack/simplify_path.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string current;
+        int i = 0;
+        int n = path.size();
+        stack<string> s;
+
+        while(i < path.size()){
+            int start = i;
+            int end = i + 1;
+            cout<<"at "<<start<<" "<<end<<endl;
+
+            while(end <= n - 1 && path[end] != '/'){
+                end++;
+            }
+            
+            current = path.substr(start, end - start);
+            cout<<current<<endl;
+            i = end;
+
+          if(current == "/." || current == "/"){
+                continue;
+            }
+
+            if(current != "/.."){
+                s.push(current);
+            }
+            else if(!s.empty()){
+                s.pop();
+            }
+
+
+        }
+
+           
+        string ans = !s.empty() ? "" : "/";
+
+        while(!s.empty()){
+            ans = s.top() + ans;
+            s.pop();
+        }
+
+        return ans;
+    }
+};
