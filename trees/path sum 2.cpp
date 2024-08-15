8ce4994393e8aa9723ff1ef4555b321e43fd6584
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-15 10:14:59 +0530

    Update path sum 2.cpp

--- trees/path sum 2.cpp
+++ trees/path sum 2.cpp
@@ -29,3 +29,40 @@ public:
         return ans;
     }
 };
+
+
+BOTH VCETORS PASSED BY REFERENCE
+
+class Solution {
+public:
+    void solve(vector<vector<int>> &ans, TreeNode* root, int target, int sum, vector<int> &curr){
+        if(root == NULL){
+            return;
+        }
+
+        sum = sum + root->val;
+        curr.push_back(root->val);
+
+        if(root->left == NULL && root->right == NULL){
+            if(sum == target){
+                ans.push_back(curr);
+            }
+            else{
+                curr.pop_back();
+                return;
+            }
+        }
+        solve(ans, root->left, target, sum, curr);
+        solve(ans, root->right, target, sum, curr);
+        
+        curr.pop_back();
+        return;
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        solve(ans, root, targetSum, 0, curr);
+
+        return ans;
+    }
+};
