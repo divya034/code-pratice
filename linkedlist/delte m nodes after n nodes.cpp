258e79d9017beb7aef412e607bc6982bc1d91ad1
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-17 12:33:32 +0530

    Update delte m nodes after n nodes.cpp

--- linkedlist/delte m nodes after n nodes.cpp
+++ linkedlist/delte m nodes after n nodes.cpp
@@ -1,4 +1,37 @@
-*/
+TWO CODES
+
+class Solution {
+public:
+    void linkdelete(struct Node **head, int n, int m) {
+        Node** temp = head;
+
+        while (*temp != NULL) {
+            // Skip m nodes
+            for(int i = 0; i < m-1; i++) {
+                if(*temp == NULL) break;
+                temp = &(*temp)->next;
+            }
+
+            if (*temp == NULL) {
+                break;
+            }
+
+            // Delete n nodes
+            for(int i = 0; i < n; i++) {
+                if(*temp == NULL || (*temp)->next == NULL) break;
+                // del of next = temp->next->next
+                Node* del = (*temp)->next;  // Get the node to delete
+                (*temp)->next = del->next;  // Bypass the node to delete
+                delete del;                 // Delete the node
+            }
+
+            temp = &(*temp)->next; 
+        }
+    }
+};
+
+
+
 class Solution {
 public:
     void linkdelete(struct Node **head, int n, int m) {
