21afcee05edfbfa801df19848c9f8b2a873c96fe
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-18 14:23:12 +0530

    Update diameter of a tree.cpp

--- trees/diameter of a tree.cpp
+++ trees/diameter of a tree.cpp
@@ -21,3 +21,39 @@ public:
         return maxi;
     }
 };
+
+
+
+O(n2)
+
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        return max(lh,rh) + 1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+
+        int totaledges = lh + rh;
+
+        int leftd = diameterOfBinaryTree(root->left);
+        int rightd = diameterOfBinaryTree(root->right);
+
+        int maxi = max(totaledges, max(leftd,rightd));
+
+        return maxi;
+
+    }
+};
