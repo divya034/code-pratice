15a881d11c134dd93d7d9761fa587b4100c9eb66
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-27 14:02:00 +0530

    Update search in a BST.cpp

--- BST/search in a BST.cpp
+++ BST/search in a BST.cpp
@@ -1,3 +1,18 @@
+2 code
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* curr = root;
+
+        while(curr != nullptr && curr->val != val){
+           curr = curr->val > val ? curr->left : curr->right; 
+        }
+
+        return curr;
+    }
+};
+
+
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
