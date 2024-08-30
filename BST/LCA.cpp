fbef6e37bcaf9f62bdb51cca79ce2ad2f3f123d2
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-30 21:24:52 +0530

    Update LCA.cpp

--- BST/LCA.cpp
+++ BST/LCA.cpp
@@ -76,3 +76,24 @@ public:
         return root;
     }
 };
+
+
+OR:
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr) {
+            return nullptr;
+        }
+        if(root->val < p->val && root->val < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        else if(root->val > p->val && root->val > q->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        else {
+            return root;
+        }
+    }
+};
