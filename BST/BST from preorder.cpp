5726145abb6b25f181ed8aafb67eb28cf7a64e63
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-30 22:26:20 +0530

    Create BST from preorder.cpp

---
+++ BST/BST from preorder.cpp
@@ -0,0 +1,27 @@
+
+
+Time Complexity: O(N), where N is the number of nodes in the BST, since each node is processed exactly once.
+Space Complexity: O(H), where H is the height of the BST, due to the recursion stack. In the worst case, this could be O(N) for a skewed tree,
+class Solution {
+public:
+    TreeNode* build(vector<int> preorder, int &index, int mini, int maxi){
+        if(index >= preorder.size() || preorder[index] < mini || preorder[index] > maxi){
+            return NULL;
+        }
+        
+        int element = preorder[index];
+        index++;
+
+        TreeNode* node = new TreeNode(element);
+
+        node->left = build(preorder, index, mini, element);
+        node->right = build(preorder, index, element, maxi);
+
+        return node;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int index = 0;
+
+        return build(preorder, index, INT_MIN, INT_MAX);
+    }
+};
