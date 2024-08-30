fac137cd9d381bcbb30b812bc31c8f0076dab4ce
divya034 <59786992+divya034@users.noreply.github.com>
2024-08-30 19:34:36 +0530

    Update insert into a BST.cpp

--- BST/insert into a BST.cpp
+++ BST/insert into a BST.cpp
@@ -1,3 +1,33 @@
+RECURSIVE:
+Time Complexity: O(H), where H is the height of the tree. This is O(log N) for a balanced tree and O(N) for a skewed tree
+Space Complexity: O(H) due to the recursion stack. This is O(log N) for a balanced tree and O(N) for a skewed tree.
+class Solution {
+public:
+    void insertNode(TreeNode* &head, int val){
+        if(head == NULL){
+            head = new TreeNode(val);
+            return;
+        }
+        
+        if(head->val > val){
+            insertNode(head->left, val);
+        }
+        else{
+            insertNode(head->right, val);
+        }
+
+        return;
+
+    }
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root == nullptr) return new TreeNode(val);
+
+        insertNode(root, val);
+        return root;
+    }
+};
+
+
 Best Case: O(log N) 
 Space Complexity: O(1)
 class Solution {
