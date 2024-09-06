15ed21801f0fd388466662166ddfba7fe51d99d8
divya034 <59786992+divya034@users.noreply.github.com>
2024-09-06 13:57:52 +0530

    Update find k closests elements.cpp

--- Heaps/find k closests elements.cpp
+++ Heaps/find k closests elements.cpp
@@ -1,5 +1,42 @@
 
 using max heap:
+TC:
+for n elements, this results in O(n log k) time to build the heap
+Sorting the final result of k elements takes O(k log k).
+= O(n log k)
+
+SC:
+he heap stores at most k elements, which takes O(k) space.
+he result vector stores k elements, which takes O(k) space.
+
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>> pq;
+
+        for(int i = 0; i<arr.size(); i++){
+            pq.push({abs(arr[i] - x), arr[i]});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+
+        vector<int> ans;
+
+        while(pq.size()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+
+without popping size willl be nlogk
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
