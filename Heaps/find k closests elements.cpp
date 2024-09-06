b6074dd1bf1cb64b643cee930dd8f6fe67fee794
divya034 <59786992+divya034@users.noreply.github.com>
2024-09-06 13:20:48 +0530

    Update find k closests elements.cpp

--- Heaps/find k closests elements.cpp
+++ Heaps/find k closests elements.cpp
@@ -1,3 +1,35 @@
+
+using max heap:
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>> pq;
+
+        for(int i = 0; i<arr.size(); i++){
+            pq.push({abs(arr[i] - x), arr[i]});
+        }
+
+        while(pq.size() > k){
+            pq.pop();
+        }
+
+        vector<int> ans;
+
+        while(pq.size()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+
+
+using minheap:
+
+
 class Solution {
 public:
     class comparator{
