c6fcc7a1faefe1ca9cd0e903885a3466675aacf3
divya034 <59786992+divya034@users.noreply.github.com>
2024-09-17 12:17:06 +0530

    Create reverse a string.cpp

---
+++ Strings/reverse a string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+//    void reversestr(vector<char>&s, int i, int j){
+//       reverse(s.begin(), s.end());
+
+//    }
+
+    void reverseString(vector<char>& s) {
+        
+        reverse(s.begin(), s.end());
+        // int j = s.size() - 1;
+        // int i = 0;
+        // reversestr(s, i, j);
+        // int l = 0;
+        // int h = s.size() - 1;
+
+        // while(l <= h){
+        //     swap(s[l], s[h]);
+        //     l++;
+        //     h--;
+        // }
+
+        // reverse(s.begin(), s.end());
+    }
+};
