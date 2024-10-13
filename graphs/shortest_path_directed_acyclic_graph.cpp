471461793e3e65adb1e0b078b9e53ef0106b69e2
divya034 <59786992+divya034@users.noreply.github.com>
2024-10-13 18:58:03 +0530

    Update shortest_path_directed_acyclic_graph.cpp

--- graphs/shortest_path_directed_acyclic_graph.cpp
+++ graphs/shortest_path_directed_acyclic_graph.cpp
@@ -1,3 +1,5 @@
+two codes:
+
 TC: 
 O(N+E) ---> Topo sort
 o(N+e) --->stack processing
@@ -94,5 +96,45 @@ void toposort(int node, vector<int> &vis, vector<pair<int,int>> adj[], stack<int
         }
         
         return dist;
+    }
+};
+
+OR
+vector<int> shortestPath(int N, int M, vector<vector<int>>& edges) {
+        // Adjacency list representation
+       vector<pair<int, int>> adj[N];  // Adjacency list for weighted graph
+    for (int i = 0; i < M; i++) {
+        int u = edges[i][0], v = edges[i][1], weight = edges[i][2];
+        adj[u].push_back({v, weight});
+    }
+
+    vector<int> dist(N, INT_MAX); 
+    queue<int> q;
+    q.push(0);
+    dist[0] = 0;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        // Explore neighbors
+       for (int j = 0; j < adj[node].size(); j++) {
+            int nextNode = adj[node][j].first;
+            int edgeWeight = adj[node][j].second;
+            
+            // Relaxation step
+            if (dist[node] + edgeWeight < dist[nextNode]) {
+                dist[nextNode] = dist[node] + edgeWeight;
+                q.push(nextNode);
+            }
+        }
+    }
+
+    // Replace unreachable nodes' distances with -1
+    for (auto &it : dist) {
+        if (it == INT_MAX) it = -1;
+    }
+    
+    return dist;
     }
 };
